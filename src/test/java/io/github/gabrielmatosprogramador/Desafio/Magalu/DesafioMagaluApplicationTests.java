7decbd9293aee2f16d815799d80a86c4ac7a06d7
gabrielmatosprogramador <gabrielmatosprogramador@gmail.com>
2025-08-24 15:10:13 -0400

    Commit Inicial do projeto

---
+++ src/test/java/io/github/gabrielmatosprogramador/Desafio/Magalu/DesafioMagaluApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.gabrielmatosprogramador.Desafio.Magalu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioMagaluApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
